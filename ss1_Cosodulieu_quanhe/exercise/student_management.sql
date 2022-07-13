1665b169c8cbf77e8405712ed627842945da397d
caotuan <caotuan1994hk@gmail.com>
2022-07-13 14:54:37 +0700

    update

---
+++ ss1_Cosodulieu_quanhe/exercise/student_management.sql
@@ -0,0 +1,18 @@
+drop database if exists bai_tap; 
+create database bai_tap;
+use bai_tap;
+create table class(
+id_class int primary key,
+`name_class` varchar(50)
+);
+create table teacher(
+id_teacher int,
+`name_teacher` varchar(50),
+age_teacher int,
+country_teacher varchar(30)
+);
+
+insert into class values(1,"Hải"),(2,"Tuấn");
+
+insert into teacher values(1,"Tuấn",24,"Đà nẵng"),(2,"Hải",22,"Đà nẵng");
+select * from class.name;
