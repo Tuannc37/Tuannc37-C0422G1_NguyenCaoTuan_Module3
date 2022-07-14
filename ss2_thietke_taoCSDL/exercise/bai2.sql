cc7e745f37ec01751f20c0452119e74c448de5d9
caotuan <caotuan1994hk@gmail.com>
2022-07-14 16:49:39 +0700

    update

--- ss2_thietke_taoCSDL/exercise/bai2.sql
+++ ss2_thietke_taoCSDL/exercise/bai2.sql
@@ -1,35 +1,35 @@
-DROP DATABASE IF EXISTS quan_ly_ban_hang;
-CREATE DATABASE quan_ly_ban_hang;
-USE quan_ly_ban_hang;
+drop database if exists quan_ly_ban_hang;
+create database quan_ly_ban_hang;
+use quan_ly_ban_hang;
 
-CREATE TABLE khach_hang (
-    ma_khach_hang INT PRIMARY KEY,
-    ten_khach_hang VARCHAR(55) NOT NULL,
-    ngay_sinh DATE
+create table customer (
+    cID int primary key,
+    cName varchar(55) not null,
+    cAge date
 );
 
-CREATE TABLE don_dat_hang (
-    ma_dat_hang INT PRIMARY KEY,
-    ma_khach_hang INT NOT NULL,
-    ngay_dat_hang DATE NOT NULL,
-    tong_tien INT NOT NULL,
-    FOREIGN KEY (ma_khach_hang)
-        REFERENCES khach_hang (ma_khach_hang)
+create table order_ (
+    oID int primary key,
+    cID int not null,
+    oDate date not null,
+    oTotalPrice int not null,
+    foreign key (cID)
+        references customer (cID)
 );
 
-CREATE TABLE san_pham (
-    ma_san_pham INT PRIMARY KEY,
-    ten_san_pham VARCHAR(30) NOT NULL,
-    gia_san_pham INT NOT NULL
+create table product (
+    pID int primary key,
+    pName varchar(30) not null,
+    pPrice int not null
 );
 
-CREATE TABLE chi_tiet_hoa_don (
-    ma_dat_hang INT NOT NULL,
-    ma_san_pham INT NOT NULL,
-    so_luong INT NOT NULL,
-    PRIMARY KEY (ma_dat_hang , ma_san_pham),
-    FOREIGN KEY (ma_dat_hang)
-        REFERENCES don_dat_hang (ma_dat_hang),
-    FOREIGN KEY (ma_san_pham)
-        REFERENCES san_pham (ma_san_pham)
+create table orderdetail (
+    oID int not null,
+    pID int not null,
+    odQTY int not null,
+    primary key (oID , pID),
+    foreign key (oID)
+        references order_ (oID),
+    foreign key (pID)
+        references product (pID)
 );
