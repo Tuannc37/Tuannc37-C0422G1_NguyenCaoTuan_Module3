6104d01edc74863f82b0d69d90d6c223d04b0884
caotuan <caotuan1994hk@gmail.com>
2022-07-18 09:56:19 +0700

    update

--- CaseStudy_Module3/casestudy_module3.sql
+++ CaseStudy_Module3/casestudy_module3.sql
@@ -221,7 +221,7 @@ values  (1,5,2,4),
  -- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
 select ho_ten
 from nhan_vien
-where (ho_ten LIKE 'H%' OR ho_ten LIKE 'T%' OR ho_ten LIKE 'K%') AND (char_length(ho_ten) <=15)
+where (ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%') and (char_length(ho_ten) <=15)
 order by ho_ten;
 
 -- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
