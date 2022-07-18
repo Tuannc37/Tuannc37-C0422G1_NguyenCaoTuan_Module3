5a1f369d6cd8abbb9e2ad7b88076ec75063dd0d3
caotuan <caotuan1994hk@gmail.com>
2022-07-18 15:30:17 +0700

    update

--- ss4_cacham_thongdung_SQL/exercise/hienthi_thongtin.sql
+++ ss4_cacham_thongdung_SQL/exercise/hienthi_thongtin.sql
@@ -1,15 +1,18 @@
 use QuanLySinhVien_ss4;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+
 select *
 from
     `Subject` s
 where
-    credit = (select 
-            max(credit)
+    s.credit = (select 
+            max(s.credit)
         from
             `subject`);
+            
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+
 select s.*
 from `Subject` s
 join Mark m on s.SubID = m.SubID
@@ -19,7 +22,9 @@ where
         from
             Mark m);
 
+
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+
 select s.*,avg(m.Mark) as diem_trung_binh
 from Student s
 left join Mark m on s.StudentID = m.StudentID
